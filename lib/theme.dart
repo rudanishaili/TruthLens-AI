615c8966f1fb354d81c16b2f797db799fb4f23fa
rudanishaili <rudanishaili@gmail.com>
2026-05-26 23:38:14 +0530

    Final Project

--- lib/theme.dart
+++ lib/theme.dart
@@ -13,7 +13,7 @@ class AppTheme {
     appBarTheme: const AppBarTheme(
       backgroundColor: Color(0xff111827),
       centerTitle: true,
-      elevation: 1,
+      elevation: 0,
     ),
 
     colorScheme: ColorScheme.dark(
